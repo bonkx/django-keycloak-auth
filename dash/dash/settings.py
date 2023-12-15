06c0acba83c705c5e31c1821ef9fdca15faab146
Farrid Putra <farridputra@gmail.com>
2023-12-15 17:24:20 +0700

    tutup login_required

--- dash/dash/settings.py
+++ dash/dash/settings.py
@@ -57,7 +57,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'login_required.middleware.LoginRequiredMiddleware',  # New
+    # 'login_required.middleware.LoginRequiredMiddleware',  # New
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 
