9eb868951c7b558fb75ea3e4d529d3abe4ff0ea7
Farrid Putra <farridputra@gmail.com>
2023-12-16 12:36:52 +0700

    vxcv

--- dash/dash/utils/my_auth.py
+++ dash/dash/utils/my_auth.py
@@ -8,7 +8,7 @@ def provider_logout(request):
     # return_to_url = request.build_absolute_uri(settings.LOGOUT_REDIRECT_URL)
     return_to_url = request.build_absolute_uri(settings.LOGIN_REDIRECT_URL)
     logout_request = \
-        f'{settings.OIDC_OP_LOGOUT_ENDPOINT}?id_token_hint={id_token}' \
+        f'{settings.OIDC_OP_LOGOUT_ENDPOINT}?client_id={settings.OIDC_RP_CLIENT_ID}' \
         f'&post_logout_redirect_uri={return_to_url}'
     # logout_request = logout_url + '?' + \
     #     urlencode({'redirect_uri': return_to_url,
