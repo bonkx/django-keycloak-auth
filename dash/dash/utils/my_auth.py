fc4fbd8cc196097ffbd8024db1e66a3e1dddbce7
Farrid Putra <farridputra@gmail.com>
2023-12-16 12:44:09 +0700

    xvcxvewe

--- dash/dash/utils/my_auth.py
+++ dash/dash/utils/my_auth.py
@@ -5,8 +5,9 @@ from django.utils.http import urlencode
 def provider_logout(request):
     id_token = str(request.COOKIES['csrftoken'])
     logout_url = settings.OIDC_OP_LOGOUT_ENDPOINT
-    return_to_url = request.build_absolute_uri(settings.LOGOUT_REDIRECT_URL)
+    # return_to_url = request.build_absolute_uri(settings.LOGOUT_REDIRECT_URL)
     # return_to_url = request.build_absolute_uri(settings.LOGIN_REDIRECT_URL)
+    return_to_url = request.build_absolute_uri(settings.LOGIN_URL)
     logout_request = \
         f'{settings.OIDC_OP_LOGOUT_ENDPOINT}?client_id={settings.OIDC_RP_CLIENT_ID}' \
         f'&post_logout_redirect_uri={return_to_url}'
