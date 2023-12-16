24f309bb2939d308168ceef6880f7ba9d0ff1de8
Farrid Putra <farridputra@gmail.com>
2023-12-16 13:03:45 +0700

    cxvcv

--- dash/dash/settings.py
+++ dash/dash/settings.py
@@ -209,7 +209,7 @@ OIDC_OP_BASE_URL = "%s/auth/realms/%s" % (KC_BASE_URI, KC_REALM)
 OIDC_OP_DISCOVERY_ENDPOINT = "%s/.well-known/openid-configuration" % (
     OIDC_OP_BASE_URL)
 
-# OIDC_RP_SIGN_ALGO = "RS256"
+OIDC_RP_SIGN_ALGO = "RS256"
 OIDC_RP_SCOPES = os.environ.get("OIDC_RP_SCOPES", "openid email profile")
 
 LOGIN_URL = "oidc_authentication_init"
