74f06dc2858c0a4d7189eddb032880ed8b1ac9e8
Farrid Putra <farridputra@gmail.com>
2023-12-16 12:11:22 +0700

    xcvcxv

--- dash/web/views/home.py
+++ dash/web/views/home.py
@@ -1,7 +1,6 @@
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-from mozilla_django_oidc.auth import OIDCAuthenticationBackend
 
 # from login_required import login_not_required
 
@@ -20,7 +19,6 @@ def index(request):
     # print("oidc_access_token : ", oidc_access_token)
     # oidc_id_token = request.session['oidc_id_token']
     # print("oidc_id_token : ", oidc_id_token)
-    print("userinfo : ", OIDCAuthenticationBackend.get_userinfo())
     print(settings.KC_BASE_URI)
     return render(request, "web/home.html", {})
 
