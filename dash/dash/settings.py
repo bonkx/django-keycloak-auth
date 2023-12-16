2fe013afa7e313e56c6a6beebcbce113cd2ca029
Farrid Putra <farridputra@gmail.com>
2023-12-16 11:55:49 +0700

    nginx

--- dash/dash/settings.py
+++ dash/dash/settings.py
@@ -228,15 +228,15 @@ OIDC_OP_JWKS_ENDPOINT = discovery_info["jwks_uri"]
 OIDC_OP_LOGOUT_ENDPOINT = discovery_info["end_session_endpoint"]
 # print(OIDC_OP_LOGOUT_ENDPOINT)
 
-LOGOUT_REDIRECT_URL = "%s?post_redirect_uri=%s" % (
+LOGOUT_REDIRECT_URL = "%s?redirect_uri=%s" % (
     OIDC_OP_LOGOUT_ENDPOINT, LOGIN_REDIRECT_URL)
 # LOGOUT_REDIRECT_URL = "%s/protocol/openid-connect/logout?client_id=%s&redirect_uri=%s" % (
 #     OIDC_OP_BASE_URL, OIDC_RP_CLIENT_ID, LOGIN_REDIRECT_URL)
 
 # OIDC_STORE_ACCESS_TOKEN = True
 # OIDC_STORE_ID_TOKEN = True
-ALLOW_LOGOUT_GET_METHOD = True
-SESSION_COOKIE_SECURE = False
+# ALLOW_LOGOUT_GET_METHOD = True
+SESSION_COOKIE_SECURE = True
 # OIDC_OP_LOGOUT_URL_METHOD = 'dash.utils.my_auth.provider_logout'
 
 #################### END KEYCLOAK CONFIG ####################
