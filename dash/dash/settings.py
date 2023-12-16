474e14a4ee3de4202e70573b7a4e9d8d0bf205e2
Farrid Putra <farridputra@gmail.com>
2023-12-16 13:15:03 +0700

    cvcxv

--- dash/dash/settings.py
+++ dash/dash/settings.py
@@ -210,7 +210,7 @@ OIDC_OP_DISCOVERY_ENDPOINT = "%s/.well-known/openid-configuration" % (
     OIDC_OP_BASE_URL)
 
 OIDC_RP_SIGN_ALGO = "RS256"
-OIDC_RP_SCOPES = os.environ.get("OIDC_RP_SCOPES", "openid email profile")
+OIDC_RP_SCOPES = os.getenv("OIDC_RP_SCOPES", "openid email profile")
 
 LOGIN_URL = "oidc_authentication_init"
 LOGIN_REDIRECT_URL = os.getenv('LOGIN_REDIRECT_URL', 'http://localhost:8000/')
