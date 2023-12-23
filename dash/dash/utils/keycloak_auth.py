918228221160cdc26a544d83219483b3c765270e
Farrid Putra <farridputra@gmail.com>
2023-12-23 21:08:22 +0700

    updaye keycloak auth

--- dash/dash/utils/keycloak_auth.py
+++ dash/dash/utils/keycloak_auth.py
@@ -2,18 +2,33 @@ from django.conf import settings
 from django.urls import reverse_lazy
 from django.utils.http import urlencode
 
+# app/auth.py
+
 
 def provider_logout(request):
-    host = "%s://%s" % (
-        request.META['wsgi.url_scheme'],
-        request.META['HTTP_HOST'],
-    )
-    # print(host)
+    """ Create the user's OIDC logout URL."""
+    # User must confirm logout request with the default logout URL
+    # and is not redirected.
     logout_url = settings.OIDC_OP_LOGOUT_ENDPOINT
-    # return_to_url = request.build_absolute_uri(settings.LOGOUT_REDIRECT_URL)
-    # return_to_url = request.build_absolute_uri(settings.LOGIN_REDIRECT_URL)
-    return_to_url = request.build_absolute_uri(host)
-    logout_request = f"{logout_url}?redirect_uri={return_to_url}"
+    logout_redirect_uri = settings.LOGIN_REDIRECT_URL
 
-    # print(logout_request)
-    return logout_request
+    # If we have the oidc_id_token, we can automatically redirect
+    # the user back to the application.
+    oidc_id_token = request.session.get('oidc_id_token', None)
+    # print(oidc_id_token)
+    if oidc_id_token:
+        logout_url = (
+            settings.OIDC_OP_LOGOUT_ENDPOINT
+            + "?"
+            + urlencode({"id_token_hint": oidc_id_token,
+                        "post_logout_redirect_uri": request.build_absolute_uri(logout_redirect_uri)})
+        )
+    else:
+        logout_url = (
+            settings.OIDC_OP_LOGOUT_ENDPOINT
+            + "?"
+            + urlencode({"client_id": settings.OIDC_RP_CLIENT_ID,
+                        "post_logout_redirect_uri": request.build_absolute_uri(logout_redirect_uri)})
+        )
+
+    return logout_url
